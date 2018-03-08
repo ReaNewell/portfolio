31d875c0dd936529d80eaa6b19030c194edde3ca
Rea Newell <patrick.r.newell@gmail.com>
2018-03-08 17:44:48 +0900

    added 'more projects' and 'more info' functionality and cleaned up styling

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,36 @@
 import React from 'react';
 
 class Project extends React.Component {
-    
+    constructor(props) {
+        super(props),
+
+        this.state = {
+            infoExtended: false
+        }
+    }
+    changeDisplay = () => {
+        if (this.state.infoExtended) {
+            this.retractInfo();
+        } else {
+            this.extendInfo();
+        }
+    }
+    extendInfo = () => {
+        this.setState(() => ({ infoExtended: true }));
+        console.log("Extend");
+    }
+    retractInfo = () => {
+        this.setState(() => ({ infoExtended: false }));
+    }
     render() {
         return (
-            <div className='homepage-project'>
+            <div className='homepage-project' onClick={this.changeDisplay}>
                 <img src={this.props.imagelocation} alt="filler image"/> <br/>
-                <a href={this.props.address} target="_blank">{this.props.name}</a>
+                <h3>{this.props.name}</h3>
                 <p>{this.props.description}</p>
+                {this.state.infoExtended && <a href={this.props.address} target="_blank">Project Website</a>}
+                {this.state.infoExtended && <a href={this.props.repository} target="_blank"><p>Github Repository</p></a>}
+                {this.state.infoExtended && <p>Skills: {this.props.skills}</p>}
             </div>
         );
     }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,30 +1,67 @@
 import React from 'react';
 import Project from './Project';
 
-const Projects = () => (
-    <div className='homepage-projects main-container'>
-        <h2>Projects</h2>
-        <div className='homepage-project-container'>
-            <Project
-                imagelocation="/img/raffl.png" 
-                name="raffl"
-                address="https://reanewell.github.io/raffl/"
-                description="Create a list of entries and randomly select one entry from the list."
-            />
-            <Project 
-                imagelocation="/img/word-counter.png"
-                name="Word Counter"
-                address="https://reanewell.github.io/word-counter/"
-                description="Type in the text area to get a count of how many words and sentences that have been entered."
-            />
-            <Project 
-                imagelocation="/img/pet-gallery.jpg"
-                name="Pet Gallery"
-                address="https://reanewell.github.io/pet-gallery/"
-                description="Pet photography webpage displaying two separte galleries."
-            />
-        </div>
-    </div>
-);
+class Projects extends React.Component {
+    constructor(props) {
+        super(props),
+
+        this.state = {
+            extraProjectsShown: false           
+        }
+    }
+    showMoreProjects = () => {
+        this.setState(() => ({ extraProjectsShown: true }));
+    }
+    showLessProjects = () => {
+        this.setState(() => ({ extraProjectsShown: false }));
+    }
+    render() {
+        return (
+            <div className='homepage-projects main-container'>
+                <h2>Projects</h2>
+                <div className='homepage-project-container'>
+                    <Project
+                        imagelocation="/img/raffl.png" 
+                        name="raffl"
+                        address="https://reanewell.github.io/raffl/"
+                        description="Create a list of entries and randomly select one entry from the list."
+                        repository="https://github.com/ReaNewell/raffl"
+                        skills="HTML CSS JavaScript"
+                    />
+                    <Project 
+                        imagelocation="/img/word-counter.png"
+                        name="Word Counter"
+                        address="https://reanewell.github.io/word-counter/"
+                        description="Type in the text area to get a count of how many words and sentences that have been entered."
+                        repository="https://github.com/ReaNewell/word-counter"
+                        skills="HTML CSS JavaScript"
+                    />
+                    <Project 
+                        imagelocation="/img/pet-gallery.jpg"
+                        name="Pet Gallery"
+                        address="https://reanewell.github.io/pet-gallery/"
+                        description="Pet photography webpage displaying two separte galleries."
+                        repository="https://github.com/ReaNewell/pet-gallery"
+                        skills="HTML CSS JavaScript"
+                    />
+                    {this.state.extraProjectsShown && 
+                        <div className='homepage-project-container'>
+                            <Project 
+                            imagelocation="/img/listlist.png"
+                            name="ListList"
+                            address="https://listlist-bd2a3.firebaseapp.com/"
+                            description="Simple list application made with React."
+                            repository="https://github.com/ReaNewell/listlist"
+                            skills="HTML CSS JavaScript React"
+                            />
+                        </div>
+                    } 
+                </div>
+                { this.state.extraProjectsShown ? <button onClick={this.showLessProjects} className="homepage-project__button">Show Less</button> : <button onClick={this.showMoreProjects} className="homepage-project__button">Show More</button> }
+            </div>
+        )
+    }   
+}
+;
 
 export default Projects;
