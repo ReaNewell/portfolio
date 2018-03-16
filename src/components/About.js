411609336acfc50e7ee801a01745124039b904ac
Rea Newell <patrick.r.newell@gmail.com>
2018-03-16 15:23:49 +0900

    updated about section

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,19 @@ const About = () => (
             <div>
                 <img src="/img/reanewell.jpeg" alt="Picture of Me"/>
                 <p>
-                    Hello! My name is Rea Newell.
-                    I am a freelancer and student of Web Design.
-                    My mission is to build websites and web applications for small businesses and personal projects!
-                    Currently I am working on building my skills in Front-End Development.
-                    I am familiar with HTML, CSS, and JavaScript.
-                    My current interests involve learning more about the React library.
+                Hello! My name is Rea Newell. 
+                I am on a mission to become a web developer. 
+                My goal is to build websites and web applications that draw in and compel their users. 
+                I have worked on building my skills in Front-End Development for the past year. 
+                I am familiar with HTML, CSS, and JavaScript. 
+                My primary framework is ReactJS, 
+                and I enjoy using other materials like Webpack and Redux.
+                <br/>
+                <br/>
+                In my own time, I enjoy spending time with my husband and pets.
+                During the day, I like to get moving with my dogs on long walks.
+                When I get back indoors my time revolves around studying new subjects and exploring the world of code.
+                I am a team player who does everything in his power to keep his peers from falling behind.
                 </p>
             </div>
         </div>
