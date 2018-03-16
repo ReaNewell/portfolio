03aafabb997938a45883400c8d52677a7a7228bb
Rea Newell <patrick.r.newell@gmail.com>
2018-03-16 15:24:42 +0900

    fixed instagram link

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -5,7 +5,7 @@ const SocialMedia = () => (
         <p>Check out some of my designs and progression of other projects on my social media.</p>
         <div className='homepage-socialmedia-container'>
             <a href="http://www.twitter.com/reanewell"><img src="/img/twitter.png" /></a>
-            <a href="http://wwww.instagram.com/reanewellwebdev"><img src="/img/instagram.jpg"/></a>
+            <a href="http://www.instagram.com/reanewellwebdev"><img src="/img/instagram.jpg"/></a>
             <a href="http://www.github.com/reanewell"><img src="/img/github.png"/></a>
             <a href="http://www.codepen.io/reanewell"><img src="/img/codepen.png"/></a>
         </div>
