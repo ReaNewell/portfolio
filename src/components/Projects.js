bdde8b7a5e8bd33c6ae39a4e9275da5886268657
Rea Newell <patrick.r.newell@gmail.com>
2018-04-02 20:48:37 +0900

    added pup-skills project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -20,20 +20,20 @@ class Projects extends React.Component {
             <div className='projects main-container' id='projects'>
                 <h2 className="subtitle">Projects</h2>
                 <div className='project-container'>
+                    <Project 
+                        imagelocation="/img/pupskills.png"
+                        name="Pup Skills"
+                        address="https://pupskills.dog"
+                        description="A web application for pet owners who want to keep track of the skills their pup has learned."
+                        repository="https://github.com/ReaNewell/pup-skills"
+                        skills="HTML CSS JavaScript React Redux Firebase Jest"
+                    />
                     <Project
                         imagelocation="/img/raffl.png" 
                         name="raffl"
                         address="https://reanewell.github.io/raffl/"
                         description="Create a list of entries and randomly select one entry from the list."
                         repository="https://github.com/ReaNewell/raffl"
-                        skills="HTML CSS JavaScript"
-                    />
-                    <Project 
-                        imagelocation="/img/word-counter.png"
-                        name="Word Counter"
-                        address="https://reanewell.github.io/word-counter/"
-                        description="Type in the text area to get a count of how many words and sentences that have been entered."
-                        repository="https://github.com/ReaNewell/word-counter"
                         skills="HTML CSS JavaScript"
                     />
                     <Project 
@@ -45,7 +45,7 @@ class Projects extends React.Component {
                         skills="HTML CSS JavaScript"
                     />
                     {this.state.extraProjectsShown && 
-                        <div className='homepage-project-container'>
+                        <div className='project-container'>
                             <Project 
                             imagelocation="/img/listlist.png"
                             name="ListList"
@@ -53,6 +53,14 @@ class Projects extends React.Component {
                             description="Simple list application made with React."
                             repository="https://github.com/ReaNewell/listlist"
                             skills="HTML CSS JavaScript React"
+                            />
+                            <Project 
+                                imagelocation="/img/word-counter.png"
+                                name="Word Counter"
+                                address="https://reanewell.github.io/word-counter/"
+                                description="Type in the text area to get a count of how many words and sentences that have been entered."
+                                repository="https://github.com/ReaNewell/word-counter"
+                                skills="HTML CSS JavaScript"
                             />
                         </div>
                     } 
