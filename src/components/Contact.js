9390e55122f46c118c73bf42fed1840257dcfbc3
Rea Newell <patrick.r.newell@gmail.com>
2018-08-14 23:18:02 -0500

    added color variables and updated designs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,6 @@ import SocialMedia from './SocialMedia';
 const Contact = () => (
     <div className='contact'>
         <div className='main-container'>
-            <h2 className='subtitle'>Contact</h2>
             <div>
                 <p className="contact-body">Get into contact with me through email: <span className="contact-body--email">patrick.r.newell@gmail.com</span></p>
             </div>
