7a9022cd330d693767f450f4c30528d3990bdff0
Rea Newell <patrick.r.newell@gmail.com>
2019-01-26 01:26:42 -0600

    Fixed low contrasting colors

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,9 +26,9 @@ class Header extends React.Component {
                     <h1 className='header__title'>Rea Newell</h1>
                     <h2 className='header__subtitle'>Front-End Web Developer</h2>
                     <div className='header-navigation'>
-                        <p className="header-navigation__link" onClick={this.scrollToProjects}>Projects</p>
-                        <p className="header-navigation__link" onClick={this.scrollToAbout}>About</p>
-                        <p className="header-navigation__link" onClick={this.scrollToContact}>Contact</p>
+                        <a className="header-navigation__link" aria-label="scroll to Projects section" onClick={this.scrollToProjects} href="#">Projects</a>
+                        <a className="header-navigation__link" aria-label="scroll to About section" onClick={this.scrollToAbout} href="#">About</a>
+                        <a className="header-navigation__link" aria-label="scroll to Contact Me section" onClick={this.scrollToContact} href="#">Contact</a>
                         <a className='header-navigation__link' href='https://blog.reanewell.com' target='_blank'>Blog</a>
                     </div>
                 </div>
