2a13d55d9aa5b15a508c9a8043900884e16f56f5
Rea Newell <patrick.r.newell@gmail.com>
2019-01-26 01:14:43 -0600

    added aria-label to social media links

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -4,10 +4,10 @@ const SocialMedia = () => (
     <div className='main-container socialmedia' id='contact'>
         <p className="socialmedia-body">Check out some of my designs and progression of other projects on my social media.</p>
         <div className='socialmedia-container'>
-            <a href="http://www.twitter.com/reanewell"><img className="socialmedia__image" src="/img/twitter.png" alt='Link to Twitter Profile'/></a>
-            <a href="http://www.instagram.com/reanewellwebdev"><img className="socialmedia__image socialmedia__image--instagram" src="/img/instagram.png" alt="Link to Instagram Profile"/></a>
-            <a href="http://www.github.com/reanewell"><img className="socialmedia__image" src="/img/github.png" alt="Link to GitHub Profile"/></a>
-            <a href="http://www.codepen.io/reanewell"><img className="socialmedia__image" src="/img/codepen.png" alt="Link to CodePen Profile"/></a>
+            <a href="http://www.twitter.com/reanewell" aria-label="Link to Twitter Profile"><img className="socialmedia__image" src="/img/twitter.png" alt='Link to Twitter Profile' aria-hidden="true"/></a>
+            <a href="http://www.instagram.com/rea.webdev" aria-label="Link to Instagram Profile"><img className="socialmedia__image socialmedia__image--instagram" src="/img/instagram.png" alt="Link to Instagram Profile" aria-hidden="true"/></a>
+            <a href="http://www.github.com/reanewell" aria-label="Link to GitHub Profile"><img className="socialmedia__image" src="/img/github.png" alt="Link to GitHub Profile" aria-hidden="true"/></a>
+            <a href="http://www.codepen.io/reanewell" aria-label="Link to CodePen Profile"><img className="socialmedia__image" src="/img/codepen.png" alt="Link to CodePen Profile" aria-hidden="true"/></a>
         </div>
     </div>
 );
