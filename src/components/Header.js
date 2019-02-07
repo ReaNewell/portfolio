5c220c90c249e73ceee34086527c67e4efe437fe
Rea Newell <patrick.r.newell@gmail.com>
2019-02-06 22:28:44 -0600

    changed navigation area from 'div' to

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,12 +25,14 @@ class Header extends React.Component {
                 <div>
                     <h1 className='header__title'>Rea Newell</h1>
                     <h2 className='header__subtitle'>Front-End Web Developer</h2>
-                    <div className='header-navigation'>
-                        <a className="header-navigation__link" aria-label="scroll to Projects section" onClick={this.scrollToProjects} href="#">Projects</a>
-                        <a className="header-navigation__link" aria-label="scroll to About section" onClick={this.scrollToAbout} href="#">About</a>
-                        <a className="header-navigation__link" aria-label="scroll to Contact Me section" onClick={this.scrollToContact} href="#">Contact</a>
-                        <a className='header-navigation__link' href='https://blog.reanewell.com' target='_blank'>Blog</a>
-                    </div>
+                    <nav>
+                        <ul className='header-navigation'>
+                            <a className="header-navigation__link" aria-label="scroll to Projects section" onClick={this.scrollToProjects} href="#">Projects</a>
+                            <a className="header-navigation__link" aria-label="scroll to About section" onClick={this.scrollToAbout} href="#">About</a>
+                            <a className="header-navigation__link" aria-label="scroll to Contact Me section" onClick={this.scrollToContact} href="#">Contact</a>
+                            <a className='header-navigation__link' href='https://blog.reanewell.com' target='_blank'>Blog</a>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         );
